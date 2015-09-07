753c26115539463a46701f9a7078074e926bbcb7
Chris Henrick <chrishenrick@gmail.com>
2015-09-07 11:10:17 -0400

    #10 add back unnest(string_to_array())

--- sql/highway_shields.sql
+++ sql/highway_shields.sql
@@ -4,7 +4,7 @@ CREATE MATERIALIZED VIEW highway_shields AS
   SELECT
     ref,
     highway,
-    (ST_DumpPoints(ST_Simplify(ST_Collect(geom), 500))).geom,
+    (ST_DumpPoints(ST_Simplify(ST_Collect(geometry), 500))).geom geometry,
     CASE WHEN ref ~ 'I \d+' THEN 1
          WHEN ref ~ 'US \d+' THEN 2
          WHEN ref ~ 'CA \d+' THEN 3
@@ -12,9 +12,9 @@ CREATE MATERIALIZED VIEW highway_shields AS
     END AS priority
   FROM (
     SELECT
-      ref,
+      unnest(string_to_array(ref, ';')) AS ref,
       highway,
-      geometry geom
+      geometry
     FROM osm_roads
     WHERE highway IN ('motorway', 'trunk', 'primary', 'secondary', 'tertiary')
   ) AS _
@@ -22,6 +22,6 @@ CREATE MATERIALIZED VIEW highway_shields AS
   GROUP BY ref, highway
   -- shuffle to ensure that routes carrying more than 1 number can display all
   -- of them
-  ORDER BY ST_GeoHash(ST_Transform(ST_SetSRID(ST_Extent(geom), 3857), 4326)), random();
+  ORDER BY ST_GeoHash(ST_Transform(ST_SetSRID(ST_Extent(geometry), 3857), 4326)), random();
 
-CREATE INDEX highway_shields_geom_gist ON highway_shields USING GIST(geom);
+CREATE INDEX highway_shields_geom_gist ON highway_shields USING GIST(geometry);
