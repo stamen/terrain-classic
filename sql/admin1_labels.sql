970bff847558b4d9b5ea3c6a17d0567b785a2164
Chris Henrick <chrishenrick@gmail.com>
2015-09-08 11:40:46 -0400

    #16 favor ST_PointOnSurface() over ST_Centroid()

--- sql/admin1_labels.sql
+++ sql/admin1_labels.sql
@@ -1,9 +1,13 @@
-CREATE TABLE ne_10m_admin_1_states_provinces_lg_centroids AS (
+-- credit: Paul Ramsey's answer to http://gis.stackexchange.com/questions/162162/postgis-query-to-retrieve-the-largest-polygon-for-multi-polygons-by-grouping-on
+
+DROP TABLE IF EXISTS ne_10m_admin_1_states_provinces_labels;
+
+CREATE TABLE ne_10m_admin_1_states_provinces_labels AS (
   WITH geoms AS (
     SELECT name, admin, scalerank, (ST_Dump(geometry)).geom AS geometry 
     FROM ne_10m_admin_1_states_provinces
   )
-  SELECT DISTINCT ON (name) name, admin, scalerank, ST_Centroid(geometry) AS geometry
+  SELECT DISTINCT ON (name) name, admin, scalerank, ST_PointOnSurface(geometry) AS geometry
   FROM geoms
   ORDER BY name ASC, ST_Area(geometry) DESC
 );
